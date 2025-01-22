2519ce169cfb67bc3d5478fdfbebf2fe30eac31b
César <csarfau@gmail.com>
2025-01-21 21:27:32 -0300

    feat: add function to add a friend to the list

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let friends = [];
+
+function adicionarAmigo() {
+  let newFriend = document.getElementById("amigo").value;
+
+  if (!newFriend) {
+    return alert("Por favor, insira um nome.");
+  } else {
+    friends.push(newFriend);
+    document.getElementById("amigo").value = "";
+  }
+}
