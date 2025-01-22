f9bbec013b8bd02d79af60e91c8fbb3976f35607
César <csarfau@gmail.com>
2025-01-21 21:28:50 -0300

    feat: add function to update a list of friends

--- app.js
+++ app.js
@@ -8,5 +8,18 @@ function adicionarAmigo() {
   } else {
     friends.push(newFriend);
     document.getElementById("amigo").value = "";
+    listaAmigos();
   }
+}
+
+function listaAmigos() {
+  let list = document.getElementById("listaAmigos");
+
+  list.innerHTML = "";
+
+  friends.forEach((friend) => {
+    let li = document.createElement("li");
+    li.textContent = friend;
+    list.appendChild(li);
+  });
 }
